a53d5b6676ee653fd846fe4502929db9c0d2cadb
MasterEat <gogosblack@gmail.com>
2026-08-01 20:43:46 +0300

    Add overlay scroll headers to photographic heroes

---
+++ assets/js/header-scroll.js
@@ -0,0 +1,25 @@
+(() => {
+    const headers = document.querySelectorAll('[data-overlay-header]');
+
+    if (!headers.length) {
+        return;
+    }
+
+    let ticking = false;
+
+    const updateHeaders = () => {
+        const isScrolled = window.scrollY > 24;
+        headers.forEach((header) => header.classList.toggle('is-scrolled', isScrolled));
+        ticking = false;
+    };
+
+    const requestUpdate = () => {
+        if (!ticking) {
+            ticking = true;
+            window.requestAnimationFrame(updateHeaders);
+        }
+    };
+
+    updateHeaders();
+    window.addEventListener('scroll', requestUpdate, { passive: true });
+})();
